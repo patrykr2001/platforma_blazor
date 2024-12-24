80cbe85c908fe7e050375b1b1480214f1df97e4a
Patryk Rakowski <95425713+patrykr2001@users.noreply.github.com>
2024-12-24 01:55:36 +0100

    Add Category and Tag models, and enhance BlogPost with new properties.

--- MyBlog/Data.Models/Models/BlogPost.cs
+++ MyBlog/Data.Models/Models/BlogPost.cs
@@ -2,5 +2,10 @@
 
 public class BlogPost
 {
-    
+    public string? Id { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public string Text { get; set; } = string.Empty;
+    public DateTime PublishedDate { get; set; }
+    public Category? Category { get; set; }
+    public List<Tag> Tags { get; set; } = new();
 }
