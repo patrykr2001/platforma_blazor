bcdfc5996f4a317c3530d23323bbd747009a1701
Patryk Rakowski <patrykr2001@gmail.com>
2025-01-03 16:31:19 +0100

    Refactor BlogApiWebClient to use specific HttpClient instances.

--- MyBlog/BlazorWebAssembly/BlazorWebAssembly.Client/BlogApiWebClient.cs
+++ MyBlog/BlazorWebAssembly/BlazorWebAssembly.Client/BlogApiWebClient.cs
@@ -2,59 +2,64 @@ using System.Net.Http.Json;
 using System.Text.Json;
 using Data.Models.Interfaces;
 using Data.Models.Models;
+using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 
 namespace BlazorWebAssembly.Client;
 
 public class BlogApiWebClient : IBlogApi
 {
-    private readonly IHttpClientFactory _factory;
+    private readonly HttpClient _publicHttpClient;
+    private readonly HttpClient _authenticatedHttpClient;
 
-    public BlogApiWebClient(IHttpClientFactory factory)
+    public BlogApiWebClient(IHttpClientFactory factory, NavigationManager navigationManager)
     {
-        _factory = factory;
+        _publicHttpClient = factory.CreateClient("Public");
+        _publicHttpClient.BaseAddress = new Uri(navigationManager.Uri);
+        _authenticatedHttpClient = factory.CreateClient("Authenticated");
+        _authenticatedHttpClient.BaseAddress = new Uri(navigationManager.Uri);
     }
 
     public async Task<int> GetBlogPostCountAsync()
     {
-        var httpClient = _factory.CreateClient("Public");
+        var httpClient = _publicHttpClient;
         return await httpClient.GetFromJsonAsync<int>("/api/BlogPostCount");
     }
 
     public async Task<List<BlogPost>?> GetBlogPostsAsync(int take, int skip)
     {
-        var httpClient = _factory.CreateClient("Public");
+        var httpClient = _publicHttpClient;
         return await httpClient.GetFromJsonAsync<List<BlogPost>>(
             $"/api/BlogPosts?numberofposts={take}&startindex={skip}");
     }
 
     public async Task<List<Category>?> GetCategoriesAsync()
     {
-        var httpClient = _factory.CreateClient("Public");
+        var httpClient = _publicHttpClient;
         return await httpClient.GetFromJsonAsync<List<Category>>("/api/Categories");
     }
 
     public async Task<List<Tag>?> GetTagsAsync()
     {
-        var httpClient = _factory.CreateClient("Public");
+        var httpClient = _publicHttpClient;
         return await httpClient.GetFromJsonAsync<List<Tag>>("/api/Tags");
     }
 
     public async Task<BlogPost?> GetBlogPostAsync(string id)
     {
-        var httpClient = _factory.CreateClient("Public");
+        var httpClient = _publicHttpClient;
         return await httpClient.GetFromJsonAsync<BlogPost>($"/api/BlogPosts/{id}");
     }
 
     public async Task<Category?> GetCategoryAsync(string id)
     {
-        var httpClient = _factory.CreateClient("Public");
+        var httpClient = _publicHttpClient;
         return await httpClient.GetFromJsonAsync<Category>($"/api/Categories/{id}");
     }
 
     public async Task<Tag?> GetTagAsync(string id)
     {
-        var httpClient = _factory.CreateClient("Public");
+        var httpClient = _publicHttpClient;
         return await httpClient.GetFromJsonAsync<Tag>($"/api/Tags/{id}");
     }
 
@@ -62,7 +67,7 @@ public class BlogApiWebClient : IBlogApi
     {
         try
         {
-            var httpClient = _factory.CreateClient("Authenticated");
+            var httpClient = _authenticatedHttpClient;
             var response = await httpClient.PutAsJsonAsync("/api/BlogPosts", item);
             var json = await response.Content.ReadAsStringAsync();
             return JsonSerializer.Deserialize<BlogPost>(json);
@@ -79,7 +84,7 @@ public class BlogApiWebClient : IBlogApi
     {
         try
         {
-            var httpClient = _factory.CreateClient("Authenticated");
+            var httpClient = _authenticatedHttpClient;
             var response = await httpClient.PutAsJsonAsync("/api/Categories", item);
             var json = await response.Content.ReadAsStringAsync();
             return JsonSerializer.Deserialize<Category>(json);
@@ -95,7 +100,7 @@ public class BlogApiWebClient : IBlogApi
     {
         try
         {
-            var httpClient = _factory.CreateClient("Authenticated");
+            var httpClient = _authenticatedHttpClient;
             var response = await httpClient.PutAsJsonAsync("/api/Tags", item);
             var json = await response.Content.ReadAsStringAsync();
             return JsonSerializer.Deserialize<Tag>(json);
@@ -111,7 +116,7 @@ public class BlogApiWebClient : IBlogApi
     {
         try
         {
-            var httpClient = _factory.CreateClient("Authenticated");
+            var httpClient = _authenticatedHttpClient;
             await httpClient.DeleteAsync($"/api/BlogPosts/{id}");
         }
         catch (AccessTokenNotAvailableException exception)
@@ -124,7 +129,7 @@ public class BlogApiWebClient : IBlogApi
     {
         try
         {
-            var httpClient = _factory.CreateClient("Authenticated");
+            var httpClient = _authenticatedHttpClient;
             await httpClient.DeleteAsync($"/api/Categories/{id}");
         }
         catch (AccessTokenNotAvailableException exception)
@@ -137,7 +142,7 @@ public class BlogApiWebClient : IBlogApi
     {
         try
         {
-            var httpClient = _factory.CreateClient("Authenticated");
+            var httpClient = _authenticatedHttpClient;
             await httpClient.DeleteAsync($"/api/Tags/{id}");
         }
         catch (AccessTokenNotAvailableException exception)
--- MyBlog/BlazorWebAssembly/BlazorWebAssembly.Client/Program.cs
+++ MyBlog/BlazorWebAssembly/BlazorWebAssembly.Client/Program.cs
@@ -1,5 +1,10 @@
+using BlazorWebAssembly.Client;
+using Data.Models.Interfaces;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+builder.Services.AddScoped<IBlogApi, BlogApiWebClient>();
+builder.Services.AddHttpClient();
 
 await builder.Build().RunAsync();
