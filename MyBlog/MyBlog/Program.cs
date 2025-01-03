a4399386d60e8e98ec52719663f5f4f432d3fc77
Patryk Rakowski <patrykr2001@gmail.com>
2025-01-03 21:02:35 +0100

    Add authentication features and integrate login/logout functionality.

--- MyBlog/MyBlog/Program.cs
+++ MyBlog/MyBlog/Program.cs
@@ -1,7 +1,11 @@
+using Auth0.AspNetCore.Authentication;
 using MyBlog.Components;
 using Data;
 using Data.Models.Interfaces;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using SharedComponents;
+using SharedComponents.RazorComponents;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -17,6 +21,12 @@ builder.Services.AddOptions<BlogApiJsonDirectAccessSettings>().Configure(options
     options.TagsFolder = "Tags";
 });
 builder.Services.AddScoped<IBlogApi, BlogApiJsonDirectAccess>();
+builder.Services.AddAuth0WebAppAuthentication(options =>
+{
+    options.Domain = builder.Configuration["Auth0:Authority"] ?? "";
+    options.ClientId = builder.Configuration["Auth0:ClientId"] ?? "";
+});
+builder.Services.AddTransient<ILoginStatus, LoginStatus>();
 
 var app = builder.Build();
 
@@ -31,10 +41,34 @@ if (!app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseStaticFiles();
+app.UseRouting();
 app.UseAntiforgery();
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode()
     .AddAdditionalAssemblies(typeof(SharedComponents._Imports).Assembly);
+
+app.MapGet("/authentication/login",
+    async (string redirectUri, HttpContext context) =>
+    {
+        var authenticationProperties = new LoginAuthenticationPropertiesBuilder()
+            .WithRedirectUri(redirectUri)
+            .Build();
+        await context.ChallengeAsync(
+            Auth0Constants.AuthenticationScheme, authenticationProperties);
+    });
+
+app.MapGet("/authentication/logout",
+    async (HttpContext context) =>
+    {
+        var authenticationProperties = new LogoutAuthenticationPropertiesBuilder()
+            .WithRedirectUri("/")
+            .Build();
+        
+        await context.SignOutAsync(Auth0Constants.AuthenticationScheme, authenticationProperties);
+        await context.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+    });
 
 app.Run();
