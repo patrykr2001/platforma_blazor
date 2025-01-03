58a66d1b403657ad3c72def2a3225a8add3e9ba0
Patryk Rakowski <patrykr2001@gmail.com>
2025-01-03 12:11:18 +0100

    Add data annotation attributes for validation in model classes.

--- MyBlog/Data.Models/Models/BlogPost.cs
+++ MyBlog/Data.Models/Models/BlogPost.cs
@@ -1,9 +1,14 @@
-﻿namespace Data.Models.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Data.Models.Models;
 
 public class BlogPost
 {
     public string? Id { get; set; }
+    [Required]
+    [MinLength(5)]
     public string Title { get; set; } = string.Empty;
+    [Required]
     public string Text { get; set; } = string.Empty;
     public DateTime PublishedDate { get; set; }
     public Category? Category { get; set; }
--- MyBlog/Data.Models/Models/Tag.cs
+++ MyBlog/Data.Models/Models/Tag.cs
@@ -1,7 +1,10 @@
-﻿namespace Data.Models.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Data.Models.Models;
 
 public class Tag
 {
     public string? Id { get; set; }
+    [Required]
     public string Name { get; set; } = string.Empty;
 }
